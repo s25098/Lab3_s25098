85cbd0a5f2c9147da5733b2e0a3adab3761e2eb9
s25098 <s25098@pjwstk.edu.pl>
2024-11-18 15:11:41 +0100

    Changing model

--- model_training.py
+++ model_training.py
@@ -1,13 +1,13 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
 
 
 def train_model(X_train, y_train):
-    model = RandomForestRegressor(random_state=42)
+    model = LinearRegression()
     model.fit(X_train, y_train)
     return model
 
@@ -17,9 +17,11 @@ def evaluate_model(model, X_test, y_test):
 
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
 
     print(f"Mean Squared Error (MSE): {mse}")
     print(f"R-squared (R²): {r2}")
+    print(f"Mean Absolute Error (MAE): {mae}")
 
     plt.figure(figsize=(8, 6))
     sns.scatterplot(x=y_test, y=y_pred)
